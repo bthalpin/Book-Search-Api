ef7ae1a622905225ca50d8d24da77c4b28ad4107
Brian Halpin <brian.t.halpin@gmail.com>
2022-04-20 09:35:39 -0400

    Update server file for apollo

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]! 
+  }
+
+  type Book {
+    _id: ID!
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String
+  }
+
+  type Auth {
+      user: User
+      token: ID!
+  }      
+
+  type Query {
+    tech: [Tech]
+    matchups(_id: String): [Matchup]
+  }
+
+  type Mutation {
+    createMatchup(tech1: String!, tech2: String!): Matchup
+    createVote(_id: String!, techNum: Int!): Matchup
+  }
+`;
+
+module.exports = typeDefs;
